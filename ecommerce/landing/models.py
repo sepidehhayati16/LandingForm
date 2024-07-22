9fa6e6096cf2ee197a91d694ef6a355a7cc42bb1
sepideh <sepideh.hayati16@Gmail.com>
2024-07-22 15:26:12 +0330

    The bootstrap file is completeÚ

--- ecommerce/landing/models.py
+++ ecommerce/landing/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MinValueValidator
 
 # Create your models here.
+class message(models.Model):
+    Fullname = models.CharField(max_length=120)
+    Email = models.EmailField()
+    phone = models.CharField(max_length=12,validators=[MinValueValidator(11),MinValueValidator(1)])
+    message = models.TextField(max_length=500)
+    def __str__(self):
+        return self.Fullname
--- ecommerce/landing/views.py
+++ ecommerce/landing/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render , redirect
+from . import models
+from django.contrib import messages
 
 # Create your views here.
+def landing(request):
+    return render(request, 'index.html')
+def sendMessage(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        phone = request.POST.get('phone')
+        message = request.POST.get('message')
+        instance = models.message.objects.create(name=name, email=email, phone=phone, message=message)
+        instance.save()
+        messages.success(request, f'Your message has been sent successfully!')
+        return redirect('landing')
+    else:
+        messages.success(request,'Your message was not sent successfully')
+        return redirect(request, 'landing')
